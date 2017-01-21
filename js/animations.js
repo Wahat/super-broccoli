f5b9874c1a9c7ece8c20700d98d8d04ccfcb5cec
James <james.sohuang@hotmail.com>
2017-01-21 04:22:15 -0500

    update

--- js/animations.js
+++ js/animations.js
@@ -2,5 +2,7 @@ $(document).ready(function() {
 	$(".nav-item").click(function() {
 		this.toggleClass("active");
 	});
-	
+	$(".typing").keypress(function() {
+		$(".pan").toggleClass("pan-up");
+	});
 });
